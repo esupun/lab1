4907dbbb54b74fa8542b9f32fc20ed1ec06a23bf
Supun Manage <supun@ymail.com>
2024-03-10 12:15:14 +0530

    for loop in mycode

--- mycode.py
+++ mycode.py
@@ -4,10 +4,11 @@ logging.basicConfig(filename="logging.log",level=logging.INFO)
 
 
 def main():
-        with open('hosts.txt') as f:
-              lines = f.readlines()
+    with open('hosts.txt') as f:
+        lines = f.readlines()
             
-    
+    for line in lines:
+        print(f"lines are  {line}")    
 
 
 if __name__ == "__main__":
